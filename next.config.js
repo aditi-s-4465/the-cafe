cc1f8d86e90a3fd1948ece038170caa275934610
aditi-s-4465 <aditi12@illinois.edu>
2026-06-09 17:16:59 -0500

    update config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'export',
+  // Serve static assets from the repo path on GitHub Pages
+  basePath: '/the-cafe',
+  assetPrefix: '/the-cafe',
 };
