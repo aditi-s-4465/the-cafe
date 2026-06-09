ee8ec9ac3c9d2da5f8ca80726b9d93cdbf227e25
aditi-s-4465 <aditi12@illinois.edu>
2026-06-09 16:18:00 -0500

    content updates

--- src/app/components/Menu/Menu.tsx
+++ src/app/components/Menu/Menu.tsx
@@ -1,12 +1,12 @@
 import './Menu.css';
 
 const menuItems = [
+    ["Not Board", "Won Most Ready to Ship Product to Market! I conducted UX research and developed the Frontend for a fullstack application to help streamline board game nights with tracking, inventory and recommendation systems."],
+    ["CYBR", "Can you beat randomness? Designed and developed a game for Dr. Kit Clement and the CATALST statistics curriclum. This game is played by hundereds of students taking STAT 200 at UIUC evey semester"],
+    ["Fruit Cutter", "Get fit and have fun! I spearheaded development for a fruit ninja like game built on React.js and tensorflow.js that let 400+ users play a fun and interactive game using their web camera"],
     ["Rhythmic React", "Play the perfect music for your workout. I lead a team to develop, design and implement an app that chooses music to play based on real time user movement speeds."],
     ["Spaced Out", "Check up on and find study spots on campus. I worked with a team to develop, design, prototype and implement a mobile-friendly web app which takes real time data about campus to help students find the perfect place to study."],
-    ["Ethicash", "Find the perfect stocks for you to invest in based on causes you care about. I designed and implemented the frontend for a web-app that let users find and create investment portfolios that aligned with causes they care about. "],
     ["Melodic Eyes", "Adding another dimension to art by playing chords based on where a user is looking. I designed and helped implement a frontend that would work with a mouse as well as an eyetracker"],
-    ["JAAMS - Sweet Tunes for you", "A quiz to choose the perfect playlist for you to wake up to. I worked on integration with the Spotipy API, development of quiz logic and creation of front-end"],
-    ["Cheffin", "Use up what's in your fridge by finding the perfect recipes. I developed and integrated a front end which allowed for advanced filtering and searching of recipes."]
 ]
 
 function Menu() {
